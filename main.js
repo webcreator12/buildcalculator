cdd1d3404d84ac6d2e395f2fb58b523b43108088
web creator <91820596+webcreator12@users.noreply.github.com>
2022-04-13 01:54:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function insert(a) {
+  document.querySelector("#text").value =
+    document.querySelector("#text").value + a;
+}
+
+function equal() {
+  document.querySelector("h1").innerHTML =
+    document.getElementById("text").value;
+  var a = document.querySelector("h1").innerHTML;
+  var text = document.querySelector("#text").value;
+  if (text.endsWith("%")) {
+    var digit = text.slice(0, -1);
+    var percentage = digit / 100;
+    document.querySelector("h1").innerHTML = percentage;
+  } else if (a) {
+    document.querySelector("h1").innerHTML = eval(a);
+  }
+}
+
+function clean() {
+  document.querySelector("h1").innerText = "";
+  document.querySelector("#text").value = "";
+}
+
+function negt() {
+  var number = document.querySelector("#text").value;
+  if (number > 0) {
+    document.querySelector("#text").value = "-" + number;
+  } else {
+    document.querySelector("#text").value = number.replace("-", "");
+  }
+}
+
+var date = new Date();
+var time = date.getHours() + ":" + date.getMinutes();
+document.querySelector("h3").innerText = time;
+
+function remove() {
+  var a = document.querySelector("#text").value;
+  document.querySelector("#text").value = a.slice(0, -1);
+}
